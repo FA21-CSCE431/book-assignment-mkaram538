2ef8566c566b849dbda84e69cf6cfcc91e1b0578
root <root@localhost>
2021-09-29 16:08:47 +0000

    CS-431 : Test Update

--- test_app/spec/feature/integration_spec.rb
+++ test_app/spec/feature/integration_spec.rb
@@ -2,11 +2,27 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a book', type: :feature do
-  scenario 'valid inputs' do
+  scenario 'valid Title' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'J. K. Rowling'
+    fill_in 'Price', with: '20'
+    fill_in 'Published date', with: '2/2/2022'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
+  end
+
+  scenario 'valid Author' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'J. K. Rowling'
+    fill_in 'Price', with: '20'
+    fill_in 'Published date', with: '2/2/2022'
+    click_on 'Create Book'
+    visit books_path
+    click_on 'Show'
+    visit show_book_path
+    expect(page).to have_content('J. K. Rowling')
   end
 end
