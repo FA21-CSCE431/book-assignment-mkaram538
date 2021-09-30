cd990ff878d52d3ee04eb1f5fd0094eaea7b93d8
mkaram538 <mkaram538@gmail.com>
2021-09-30 11:12:18 -0500

    CS-431 : Tests Update

--- test_app/spec/feature/integration_spec.rb
+++ test_app/spec/feature/integration_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe 'Creating a book', type: :feature do
     fill_in 'Title', with: 'harry potter'
     fill_in 'Author', with: 'J. K. Rowling'
     fill_in 'Price', with: '20'
-    select '30', :from => 'book[date_published(3i)]'
-	select 'September', :from => 'book[date_published(2i)]'
-	select '2021', :from => 'book[date_published(1i)]'
+    select '30', :from => 'book[published_date(3i)]'
+	select 'September', :from => 'book[published_date(2i)]'
+	select '2021', :from => 'book[published_date(1i)]'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
@@ -20,9 +20,9 @@ RSpec.describe 'Creating a book', type: :feature do
     fill_in 'Title', with: 'harry potter'
     fill_in 'Author', with: 'J. K. Rowling'
     fill_in 'Price', with: '20'
-    select '30', :from => 'book[date_published(3i)]'
-	select 'September', :from => 'book[date_published(2i)]'
-	select '2021', :from => 'book[date_published(1i)]'
+    select '30', :from => 'book[published_date(3i)]'
+	select 'September', :from => 'book[published_date(2i)]'
+	select '2021', :from => 'book[published_date(1i)]'
     click_on 'Create Book'
     visit books_path
     click_on 'Show'
@@ -34,9 +34,9 @@ RSpec.describe 'Creating a book', type: :feature do
     fill_in 'Title', with: 'harry potter'
     fill_in 'Author', with: 'J. K. Rowling'
     fill_in 'Price', with: '20'
-    select '30', :from => 'book[date_published(3i)]'
-	select 'September', :from => 'book[date_published(2i)]'
-	select '2021', :from => 'book[date_published(1i)]'
+    select '30', :from => 'book[published_date(3i)]'
+	select 'September', :from => 'book[published_date(2i)]'
+	select '2021', :from => 'book[published_date(1i)]'
     click_on 'Create Book'
     visit books_path
     click_on 'Show'
@@ -48,12 +48,12 @@ RSpec.describe 'Creating a book', type: :feature do
     fill_in 'Title', with: 'harry potter'
     fill_in 'Author', with: 'J. K. Rowling'
     fill_in 'Price', with: '20'
-    select '30', :from => 'book[date_published(3i)]'
-	select 'September', :from => 'book[date_published(2i)]'
-	select '2021', :from => 'book[date_published(1i)]'
+    select '30', :from => 'book[published_date(3i)]'
+	select 'September', :from => 'book[published_date(2i)]'
+	select '2021', :from => 'book[published_date(1i)]'
     click_on 'Create Book'
     visit books_path
     click_on 'Show'
-    expect(page).to have_content('2021-02-02')
+    expect(page).to have_content('2021-09-30')
   end
 end
