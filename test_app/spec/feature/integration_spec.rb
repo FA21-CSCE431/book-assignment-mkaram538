ce26ee4b913c2fe4d5cae47ead34124dfecaf6e1
mkaram538 <mkaram538@gmail.com>
2021-09-30 11:10:38 -0500

    CS-431 : Tests Update

--- test_app/spec/feature/integration_spec.rb
+++ test_app/spec/feature/integration_spec.rb
@@ -20,7 +20,9 @@ RSpec.describe 'Creating a book', type: :feature do
     fill_in 'Title', with: 'harry potter'
     fill_in 'Author', with: 'J. K. Rowling'
     fill_in 'Price', with: '20'
-    fill_in 'Published date', with: '2/2/2022'
+    select '30', :from => 'book[date_published(3i)]'
+	select 'September', :from => 'book[date_published(2i)]'
+	select '2021', :from => 'book[date_published(1i)]'
     click_on 'Create Book'
     visit books_path
     click_on 'Show'
